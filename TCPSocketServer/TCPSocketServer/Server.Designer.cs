bcf71d8c0698a771f97b4a00c54d3e57932e856b
lockgo <juan.t.chen@gmail.com>
2019-10-07 23:13:54 -0400

    initial setup
    initial file upload.

---
+++ TCPSocketServer/TCPSocketServer/Server.Designer.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Crogogo
+ * Date: 10/6/2019
+ * Time: 10:24 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TCPSocketServer
+{
+	partial class Server
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button btnStartServer;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnStartServer = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// btnStartServer
+			// 
+			this.btnStartServer.Location = new System.Drawing.Point(12, 238);
+			this.btnStartServer.Name = "btnStartServer";
+			this.btnStartServer.Size = new System.Drawing.Size(75, 23);
+			this.btnStartServer.TabIndex = 0;
+			this.btnStartServer.Text = "Start Server";
+			this.btnStartServer.UseVisualStyleBackColor = true;
+			this.btnStartServer.Click += new System.EventHandler(this.BtnStartServerClick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(93, 238);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(268, 220);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			// 
+			// Server
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 273);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnStartServer);
+			this.Name = "Server";
+			this.Text = "Server";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ TCPSocketServer/TCPSocketServer/Server.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Crogogo
+ * Date: 10/6/2019
+ * Time: 10:24 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TCPSocketServer
+{
+	/// <summary>
+	/// Description of Server.
+	/// </summary>
+	public partial class Server : Form
+	{
+		
+		private StreamWriter serverStreamWriter;
+        private StreamReader serverStreamReader;
+		public Server()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		private bool StartServer()
+        {
+            //create server's tcp listener for incoming connection
+            TcpListener tcpServerListener = new TcpListener(4444);
+            tcpServerListener.Start();        //start server
+            Console.WriteLine("Server Started");
+            this.btnStartServer.Enabled = false;
+            //block tcplistener to accept incoming connection
+            Socket serverSocket = tcpServerListener.AcceptSocket();
+ 
+            try
+            {
+                if (serverSocket.Connected)
+                {
+                   Console.WriteLine("Client connected");
+                   //open network stream on accepted socket
+                   NetworkStream serverSockStream = 
+                       new NetworkStream(serverSocket);
+                   serverStreamWriter =  new StreamWriter(serverSockStream);
+                   serverStreamReader =  new StreamReader(serverSockStream);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.StackTrace); 
+                return false;
+            }
+ 
+            return true;
+        }
+		
+		void BtnStartServerClick(object sender, EventArgs e)
+		{
+			//start server
+            if (!StartServer())
+            {
+            	Console.WriteLine("Unable to start server");
+            	textBox1.Text = "Unable to start server";
+            }
+                
+ 
+            //sending n receiving msgs
+            while (true)
+            {
+                //Console.WriteLine("CLIENT: "+serverStreamReader.ReadLine());
+                
+                richTextBox1.Text = richTextBox1.Text + "\n" + " " + serverStreamReader.ReadLine();
+                serverStreamWriter.WriteLine("Hi! " + richTextBox1.Text); 
+                serverStreamWriter.Flush();
+            }
+		}
+	}
+}
